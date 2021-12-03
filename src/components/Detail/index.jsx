534c1e42e3f5e700aab0db9cbe23dac4f76398f1
Lucie Musilová <musil.lucie@seznam.cz>
2021-12-03 16:00:16 +0100

    in progress

---
+++ src/components/Detail/index.jsx
@@ -0,0 +1,45 @@
+import React, {useState, useEffect} from 'react';
+import { useParams } from 'react-router-dom';
+
+import './style.css';
+
+const Detail = () => {
+  const {id} = useParams();
+  const [item, setItem] = useState([]);
+  let i = 0;
+
+  const fetchData = () => {
+    fetch(`https://raw.githubusercontent.com/Czechitas-React-podklady/superkviz-api/main/quiz/${id}.json`)
+    .then(response => response.json())
+    .then(data => {
+      setItem(data);
+    })
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  console.log(item)
+
+  return (
+    <div className="question">
+
+      <p className="question__number">Otázka {item.questions[i]} / {item.length}</p>
+
+      <h2 className="question__title">{item.questions[i].title}</h2>
+
+      <div className="question__content">
+        <img className="question__image" src={item.questions[i].image} alt="Ilustrační obrázek"/>
+
+        <div className="question__answers">
+          <button className="question__answer">{item.questions[i].answers[0]}</button>
+          <button className="question__answer">{item.questions[i].answers[1]}</button>
+          <button className="question__answer">{item.questions[i].answers[2]}</button>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Detail;
--- src/index.jsx
+++ src/index.jsx
@@ -2,17 +2,33 @@ import React from 'react';
 import { render } from 'react-dom';
 import './style.css';
 
-const App = () => (
-  <>
-    {/*
-      Tady bude tvůj Superkvíz :)
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-      - HTML/CSS podklady máš ve složce /html-vzor
-      - vyzobej si ze souborů HTML a CSS pro svoje komponenty
-      - aplikace má nejméně 4 stránky (úvod, přehled kvízů, detail kvízu, žebříček)
-      - použij router (musíš si ho nainstalovat)
-    */}
-  </>
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
+import Kvizy from './components/Kvizy'
+import Zebricek from './components/Zebricek'
+import Detail from './components/Detail'
+
+const App = () => (
+  <BrowserRouter>
+    <Header />
+
+    <main className="main">
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/kvizy" element={<Kvizy />} />
+        <Route path="/kvizy/:id" element={<Detail />} />
+        <Route path="/zebricek" element={<Zebricek />} />
+        
+      </Routes>
+    </main>
+    
+    <Footer />
+    
+  </BrowserRouter>
+
 );
 
 render(<App />, document.querySelector('#app'));
